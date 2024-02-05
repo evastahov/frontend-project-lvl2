cee154b7c0604df1cc1b9a66af04368d5937213d
evastahov <majik48ru@gmail.com>
2024-02-05 21:10:21 +0300

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,15 @@
+#!/usr/bin/env node
+
 import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .helpOption('-h, --help', 'output usage information')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+
+
 
 program.parse();

97f6cc0696ff8f5c6ee68463e1c58997e5b41536
evastahov <majik48ru@gmail.com>
2024-02-07 20:25:17 +0300

    step4

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import { readFileSync } from 'fs';
+import _ from 'lodash';
 
 const getData = (filePath) => readFileSync(filePath);
 
 const genDiff = (filePath1, filePath2) => {
   const data1 = JSON.parse(getData(filePath1));
   const data2 = JSON.parse(getData(filePath2));
-  return {...data1, ...data2}
-}
+  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
+  const result = keys.map((key) => {
+    if (Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
+      return data1[key] === data2[key]
+        ? `    ${key}: ${data1[key]}`
+        : `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+    } else if (Object.hasOwn(data1, key)) {
+      return `  - ${key}: ${data1[key]}`;
+    } else if (Object.hasOwn(data2, key)) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+  });
+  return `{\n${result.join('\n')}\n}`
+ };
 
 export default genDiff;
-
-
-
-

e5e2bd8aadc29ca8c7dc039a46a6425ae2482e10
evastahov <majik48ru@gmail.com>
2024-02-09 08:26:34 +0300

    step5

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,17 +1,14 @@
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import genDiff from '../src/index.js';
 import { test, expect } from '@jest/globals';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath((import.meta.url));
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8').trim();
 
 test('genDiff', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('result.txt'));
 });
-
-// console.log(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')));
-// console.log(readFile('result.txt'));

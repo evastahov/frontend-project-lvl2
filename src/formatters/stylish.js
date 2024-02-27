44557aba858294c40b2aa905cd19056e725811d3
evastahov <majik48ru@gmail.com>
2024-02-27 22:41:21 +0300

    step9

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,4 +1,14 @@
-const createIndent = (acc) => '.'.repeat(acc * 4 - 2);
+import _ from 'lodash';
+
+const createIndent = (acc) => ' '.repeat(acc * 4 - 2);
+
+const createObjLine = (node, depth = 1) => {
+  if (!_.isObject(node)) {
+    return node;
+  }
+  const result = Object.entries(node).map(([key, value]) => `${createIndent(depth + 1)}  ${key}: ${createObjLine(value, depth + 1)}`);
+  return `{\n${result.join('\n')}\n  ${createIndent(depth)}}`;
+};
 
 const createLine = (data, depth = 1) => {
   const result = data.map((node) => {
@@ -6,16 +16,15 @@ const createLine = (data, depth = 1) => {
       case 'nested':
         return `${createIndent(depth)}  ${node.key}: {\n${createLine(node.children, depth + 1)}\n${createIndent(depth)}  }`;
       case 'added':
-        return `${createIndent(depth)}+ ${node.key}: ${node.value}`;
+        return `${createIndent(depth)}+ ${node.key}: ${createObjLine(node.value)}`;
       case 'deleted':
-        return `${createIndent(depth)}- ${node.key}: ${node.value}`;
+        return `${createIndent(depth)}- ${node.key}: ${createObjLine(node.value)}`;
       case 'changed':
-        return `${createIndent(depth)}- ${node.key}: ${node.value}\n${createIndent(depth)}+ ${node.key}: ${node.value2}`;
+        return `${createIndent(depth)}- ${node.key}: ${createObjLine(node.value)}\n${createIndent(depth)}+ ${node.key}: ${createObjLine(node.value2)}`;
       default:
-        return `${createIndent(depth)}  ${node.key}: ${node.value}`;
+        return `${createIndent(depth)}  ${node.key}: ${createObjLine(node.value)}`;
     }
   });
-
   return result.join('\n');
 };
 

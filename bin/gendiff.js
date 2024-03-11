fd03388e2ecf53bc04930f75b853ae37430006ed
evastahov <majik48ru@gmail.com>
2024-03-11 18:53:53 +0300

    add default value

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,7 @@ import genDiff from '../src/index.js';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
